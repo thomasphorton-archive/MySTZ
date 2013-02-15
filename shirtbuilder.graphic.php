82bc2758880c1fdafffe7c76cc4d729270de6794
Tom Horton <thomasphorton@gmail.com>
2013-02-15 14:47:40 -0500

    Added randomize function to graphic shirtbuilder.

--- shirtbuilder.graphic.php
+++ shirtbuilder.graphic.php
@@ -3,6 +3,8 @@
 	$meta_description = "build a one-of-a-kind piece of art. then wear it.";
 	include 'inc.header.html.php';
 	include 'inc.header.php'; 
+  include 'shirtbuilder.shirts.php';
+  include 'shirtbuilder.graphicoptions.php'; 
 ?>
 <link rel="stylesheet" type="text/css" href="css/shirtmaker.css" />
 <link rel="stylesheet" href="libraries/flexslider/flexslider.css" type="text/css" />
@@ -48,165 +50,35 @@
       controlsContainer: ".flex-container"
     });
 		
+		var designSlider = $('.graphicslide').data('flexslider');
+		var colorSlider = $('.colorslide').data('flexslider');
+		
+		num_designs = <?=count($design_options)?>;		
+		num_shirts = <?=count($shirt_color_options)?>;
+		
+		randomize = $('.shirtmaker-randomize');
+		randomize.click(function(){
+			rand_design = Math.ceil(Math.random() * num_designs);
+			rand_shirt = Math.ceil(Math.random() * num_shirts);
+			designSlider.flexAnimate(rand_design - 1);
+			colorSlider.flexAnimate(rand_shirt - 1);
+			graphic = graphicControl.filter(':nth-child(' + rand_design + ')').data('graphic');
+			color = colorControl.filter(':nth-child(' + rand_shirt + ')').data('color');
+			customString = color + ' ' + graphic;
+	    customDetail.val(customString);
+		});
+		
 	 });
+	 
+	 
 </script>
 
-<?
-    include ('shirtbuilder.shirts.php');
-      
-      $design_options = array(
-      	"Beast Mode" => array(
-       		"title" => "Beast Mode",
-       		"full_size" => "beastmode.black.png",
-       		"thumb" => "beastmode.black.thumb.png",
-       	),
-       	"Daps n Pounds" => array(
-       		"title" => "Daps n Pounds",
-       		"full_size" => "dapsnpounds.grey.png",
-       		"thumb" => "dapsnpounds.grey.thumb.png",
-       	),
-       	"Double STZ Burger" => array(
-       		"title" => "Double STZ Burger",
-       		"full_size" => "doublestzburger.black.png",
-       		"thumb" => "doublestzburger.black.thumb.png",
-       	),
-       	"El Caputo" => array(
-       		"title" => "El Caputo",
-       		"full_size" => "elcaputo.black.png",
-       		"thumb" => "elcaputo.black.thumb.png",
-       	),
-       	"Emergency Mustache" => array(
-       		"title" => "Emergency Mustache",
-       		"full_size" => "emergencymustache.black.png",
-       		"thumb" => "emergencymustache.black.thumb.png",
-       	),/*
-       	"En Garde" => array(
-       		"title" => "En Garde",
-       		"full_size" => "engarde.black.png",
-       		"thumb" => "engarde.black.thumb.png",
-       	),*/
-       	"Gorilla Boombox" => array(
-       		"title" => "Gorilla Boombox",
-       		"full_size" => "gorillaboombox.black.png",
-       		"thumb" => "gorillaboombox.black.thumb.png",
-       	),
-       	"Skyline" => array(
-       		"title" => "Skyline",
-       		"full_size" => "skyline.black.png",
-       		"thumb" => "skyline.black.thumb.png",
-       	),
-       	"I Skate NC" => array(
-       		"title" => "I Skate NC",
-       		"full_size" => "iskatenc.black.png",
-       		"thumb" => "iskatenc.black.thumb.png",
-       	),
-       	"I Skate NC" => array(
-       		"title" => "I Skate NC (Blue)",
-       		"full_size" => "iskatenc.blue.png",
-       		"thumb" => "iskatenc.blue.thumb.png",
-       	),
-       	"Moostache" => array(
-       		"title" => "Moostache",
-       		"full_size" => "moostache.black.png",
-       		"thumb" => "moostache.black.thumb.png",
-       	),
-       	"Party Animals" => array(
-       		"title" => "Party Animals",
-       		"full_size" => "partyanimals.grey.png",
-       		"thumb" => "partyanimals.grey.thumb.png",
-       	),
-       	"Peace" => array(
-       		"title" => "Peace (Black)",
-       		"full_size" => "peace.black.png",
-       		"thumb" => "peace.black.thumb.png",
-       	),
-       	"Peace" => array(
-       		"title" => "Peace (Blue)",
-       		"full_size" => "peace.blue.png",
-       		"thumb" => "peace.blue.thumb.png",
-       	),
-       	"Ride NC Black" => array(
-       		"title" => "Ride NC (Black)",
-       		"full_size" => "ridenc.black.png",
-       		"thumb" => "ridenc.black.thumb.png",
-       	),
-       	"Ride NC Orange" => array(
-       		"title" => "Ride NC (Orange)",
-       		"full_size" => "ridenc.orange.png",
-       		"thumb" => "ridenc.orange.thumb.png",
-       	),
-       	"Right Coast" => array(
-       		"title" => "Right Coast",
-       		"full_size" => "rightcoast.black.png",
-       		"thumb" => "rightcoast.black.thumb.png",
-       	),
-       	"Shoot All Shred" => array(
-       		"title" => "Shoot All Shred",
-       		"full_size" => "shootallshred.black.png",
-       		"thumb" => "shootallshred.black.thumb.png",
-       	),
-       	"Shred Til You're Dead" => array(
-       		"title" => "Shred Til You're Dead",
-       		"full_size" => "shredtilyouredead.black.png",
-       		"thumb" => "shredtilyouredead.black.thumb.png",
-       	),
-       	"Triangles" => array(
-       		"title" => "Triangles",
-       		"full_size" => "triangles.black.png",
-       		"thumb" => "triangles.black.thumb.png",
-       	),
-       	"Hand Shredded" => array(
-       		"title" => "Hand Shredded",
-       		"full_size" => "handshredded.black.png",
-       		"thumb" => "handshredded.black.thumb.png",
-       	),
-       	"We Can Do It" => array(
-       		"title" => "We Can Do It",
-       		"full_size" => "wecandoit.black.png",
-       		"thumb" => "wecandoit.black.thumb.png",
-       	),
-       	"Happy Shredding" => array(
-       		"title" => "Happy Shredding",
-       		"full_size" => "happyshredding.orange.png",
-       		"thumb" => "happyshredding.orange.thumb.png",
-       	),
-       	"KJ Gorilla" => array(
-       		"title" => "KJ Gorilla",
-       		"full_size" => "kjgorilla.black.png",
-       		"thumb" => "kjgorilla.black.thumb.png",
-       	),
-       	"OG Whaler Black" => array(
-       		"title" => "OG Whaler (Black)",
-       		"full_size" => "ogwhaler.black.png",
-       		"thumb" => "ogwhaler.black.thumb.png",
-       	),
-       	"OG Whaler White" => array(
-       		"title" => "OG Whaler (White)",
-       		"full_size" => "ogwhaler.white.png",
-       		"thumb" => "ogwhaler.white.thumb.png",
-       	),
-       	"Slush Buddie" => array(
-       		"title" => "Slush Buddie (White)",
-       		"full_size" => "slushbuddie.white.png",
-       		"thumb" => "slushbuddie.white.thumb.png",
-       	),
-       	"STZ Life" => array(
-       		"title" => "STZ Life",
-       		"full_size" => "stzlife.black.png",
-       		"thumb" => "stzlife.black.thumb.png",
-       	),
-       	"Too Fresh" => array(
-       		"title" => "Too Fresh",
-       		"full_size" => "toofresh.blue.png",
-       		"thumb" => "toofresh.blue.thumb.png",
-       	),
-      );
-       ?>
+
 
 <div class="page-wrapper shirtMaker">
 	<div id="shirtViewer">
   	<div class="slider">
-    	<div id="colorOptions" class="flexslider" >
+    	<div id="colorOptions" class="flexslider colorslide" >
       	<ul class="slides">
         	<?	foreach ($shirt_color_options as $color){	?>
           	<li><img src="images/shirtbuilder/blanks/<?=$color["shirt"]?>" alt="<?=$color["title"]?>" /></li>
@@ -224,6 +96,8 @@
       </div><!--.flexslider1-->
     </div><!--.slider-->
   </div><!--#shirtMaker--> 
+  
+  <span class="shirtmaker-randomize" style="float: right;" onclick="_gaq.push(['_trackEvent', 'Custom Shirt', 'Randomized', 'Details to Be Added']);">Randomize</span>
   
   <div class="flex-container shirtmaker-step" id="designContainer">
 		<h2 class="step1">1. Pick Your Graphic</h2>
@@ -269,7 +143,7 @@
 			</table>
 			<input type="hidden" name="currency_code" value="USD">
 			
-			<button type="submit" class="btn" name="submit" onclick="_gaq.push(['_trackEvent', 'Add to Cart', 'Custom Graphic', 'Details to Be Added');">Add to Cart</button>
+			<button type="submit" class="btn" name="submit" onclick="_gaq.push(['_trackEvent', 'Add to Cart', 'Custom Graphic', 'Details to Be Added']);">Add to Cart</button>
 		
 			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 		</form>

74c0bcf4f85d16af6dfcf8a1e122bb28f9709c49
Tom Horton <thomasphorton@gmail.com>
2013-12-13 18:55:36 -0500

    changes to includes to allow custom header.

--- inc.header.html.php
+++ inc.header.html.php
@@ -3,6 +3,7 @@
 <head>
 <title><?=$title?></title>
 <meta http-equiv="Content-Type" Content="text/html; charset=UTF-8">
+<meta charset="UTF-8" />
 <meta property="og:image" content="http://beta.mystz.com/images/index/instagram-logo.png" />
 <meta property="og:site_name" content="MySTZ"/>
 <meta property="og:description" content="Get the latest and greatest custom gear for wake, skate, surf and snow" />
